0475d4fe5d5d8ad3ca89ccc05d70e533cc48a188
AlinaKhalilova <akhalilova@alumni.escaet.fr>
2018-02-14 15:51:28 +0100

    exo1 : restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,1 +1,47 @@
 
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def top
+    @restaurants = Restaurant.where(stars: 5)
+  end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+  end
+
+
+  def create
+    @restaurant = Restaurant.new
+    @restaurant.save
+  end
+
+
+  def update
+    respond_to do |format|
+      if @restaurant.update
+        redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  end
+
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
+  end
+
+
+end
